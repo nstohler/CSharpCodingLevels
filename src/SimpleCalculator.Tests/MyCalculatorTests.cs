2e604f6f8b1f3eeb5280aba73cda148964c09875
Nicolas Stohler <nstohler@gmail.com>
2021-10-12 09:27:10 +0200

    provide sample unit tests

---
+++ src/SimpleCalculator.Tests/MyCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class MyCalculatorTests
+    {
+        [TestMethod]
+        public void SetCalculatorName_sets_calculator_name_and_GetCalculatorName_gets_name()
+        {
+            // Arrange
+            var name = "The ultimate calculator ⚡";
+            var calculator = new MyCalculator();
+            calculator.GetCalculatorName().Should().NotBe(name);
+
+            // Act
+            calculator.SetCalculatorName(name);
+
+            // Assert
+            calculator.GetCalculatorName().Should().Be(name);
+        }
+    }
+}
---
+++ src/SimpleCalculator/MyCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class MyCalculator
+    {
+        private string CalculatorName;
+
+        public void SetCalculatorName(string name)
+        {
+            CalculatorName = name;
+        }
+
+        public string GetCalculatorName()
+        {
+            return CalculatorName;
+        }
+    }
+}
