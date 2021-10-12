e1a4189bcf41b12dcb06402aef2017819a0aae24
Nicolas Stohler <nstohler@gmail.com>
2021-10-12 15:03:59 +0200

    fix comment

--- src/SimpleCalculator/MyCalculator.cs
+++ src/SimpleCalculator/MyCalculator.cs
@@ -136,7 +136,7 @@ namespace SimpleCalculator
         /// </summary>
         public List<int> GetFibonacciNumbers(int count)
         {
-            // Fibonacci numbers: 1, 2, 3, 5, 8, 13, 21, 34, ...
+            // Fibonacci numbers: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
             var fibonacciNumbers = new List<int>();
 
