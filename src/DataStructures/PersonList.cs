ff7b707182405acb44c7bf1670ac407f2e61090b
Nicolas Stohler <Nicolas.Stohler@varian.com>
2021-10-15 12:17:11 +0200

    implement person list methods

--- src/DataStructures/PersonList.cs
+++ src/DataStructures/PersonList.cs
@@ -12,17 +12,106 @@ namespace DataStructures
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return _firstElement == null;
         }
 
         public int Count()
         {
-            throw new NotImplementedException();
+            PersonListElement currentElement = _firstElement;
+            var count = 0;
+
+            while (currentElement != null)
+            {
+                currentElement = currentElement.NextElement;
+                count++;
+            }
+
+            return count;
         }
 
         public PersonListElement Add(Person person)
         {
-            throw new NotImplementedException();
+            var addedElement = new PersonListElement()
+            {
+                Person = person,
+                NextElement = null
+            };
+
+            // first?
+            if (_firstElement == null)
+            {
+                _firstElement = addedElement;
+            }
+            else
+            {
+                var nextElement = _firstElement;
+                while (nextElement.NextElement != null)
+                {
+                    nextElement = nextElement.NextElement;
+                }
+                nextElement.NextElement = addedElement;
+            }
+            
+            return addedElement;
+        }
+
+        public PersonListElement GetAt(int index)
+        {
+            var count = Count();
+            if (index < 0 || index >= count)
+            {
+                return null;
+            }
+
+            var elementIndex = 0;
+            var element = _firstElement;
+            while (elementIndex != index)
+            {
+                element = element.NextElement;
+                elementIndex++;
+            }
+
+            return element;
+        }
+
+        public PersonListElement InsertAt(int index, Person person)
+        {
+            var count = Count();
+            if (index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Invalid index: {index}");
+            }
+
+            var insertElement = new PersonListElement()
+            {
+                Person = person,
+                NextElement = null
+            };
+            
+            if (index == 0)
+            {
+                insertElement.NextElement = _firstElement;
+                _firstElement = insertElement;
+            }
+            else
+            {
+                var elementIndex = 0;
+                var nextElement = _firstElement;
+                PersonListElement previousElement = null;
+                while (elementIndex != index)
+                {
+                    previousElement = nextElement;
+                    nextElement = nextElement.NextElement;
+                    elementIndex++;
+                }
+                if(previousElement != null)
+                {
+                    previousElement.NextElement = insertElement;
+                }
+                insertElement.NextElement = nextElement;
+            }
+
+            return insertElement;
         }
 
     }
