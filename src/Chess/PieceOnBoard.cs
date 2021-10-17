cb0badf63de5d37c1b621a0b65f90622f100434d
Nicolas Stohler <nstohler@gmail.com>
2021-10-17 17:50:38 +0200

    add some notes

--- src/Chess/PieceOnBoard.cs
+++ src/Chess/PieceOnBoard.cs
@@ -18,5 +18,11 @@ namespace Chess
         // TODO: 
         // - needs info of other pieces to update attack highlights
         // - move piece?
+        // - color endangered pieces in different color
+        // - white/black player distinguishing
+
+        // REQ?
+        // - Helper service
+        //   - To check if one position is horizontally/vertically/diagonally reachable/attackable from another position
     }
 }
